1849d85b428059b54cf42b36d71d6f5035667b84
Samuel Lam <samuelkinlam@gmail.com>
2017-03-18 21:15:26 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Strassen's Algorithm
---
+++ main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//
+//  Created on 3/18/17.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
